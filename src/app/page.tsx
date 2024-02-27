d8db4cc35354fac3adda6e3622c3153b62620502
abkumar9677 <abhi967792@gmail.com>
2024-02-27 12:55:23 +0530

    commiting apllication setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,16 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+      <br />
+      <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
